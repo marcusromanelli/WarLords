90fffb3d6c2e9a4313ec37b5b4961afeadc2c0b1
Marcus Romanelli <marcus.romanelli.m@gmail.com>
2023-02-21 16:47:18 -0300

    [BugFix] Fixes a problem that halts ther scedne loader if one tries to do a screen reload while a screen was benig reloaded
    Also SceneController refactor

--- Assets/_WarLords/Scripts/SceneController/SceneController.cs
+++ Assets/_WarLords/Scripts/SceneController/SceneController.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class SceneController : Singleton<SceneController>
 {
+	static bool IsChangingScene = false;
 #if UNITY_EDITOR
 	void Update()
     {
@@ -12,36 +14,59 @@ public class SceneController : Singleton<SceneController>
 #endif
 	public static void LoadLevel(MenuScreens level = MenuScreens.Loader, float fadeOt = 0.5f, float fadeIn = 0.5f)
 	{
+		if (IsChangingScene)
+			return;
+
 		Instance._loadLevel(level, fadeOt, fadeIn);
 	}
 	void _loadLevel(MenuScreens level, float fadeOt, float fadeIn)
 	{
 		StartCoroutine(_loadLevel((int)level, fadeOt, fadeIn));
 	}
-	/*public void LoadLevel(StageNames level = StageNames.Swamp, float fadeout = 0.5f, float fadein = 0.5f)
-	{
-		int number = Enum.GetNames(typeof(MenuScreens)).Length - 1;
-		StartCoroutine(_loadLevel((int)level + number + 1, fadeout, fadein));
-	}*/
+
 	IEnumerator _loadLevel(int level, float fadeOt, float fadeIn)
 	{
+		IsChangingScene = true;
+
 		ScreenController.showLoadingIcon = true;
-		ScreenController.FadeOut(fadeOt);
 
-		while (ScreenController.Instance.currentFade < 1)
-		{
-			yield return null;
-		}
+		yield return DoFadeOut(fadeOt);
 
+		yield return LoadScene(level);
+
+		yield return DoFadeIn(fadeIn);
+
+		IsChangingScene = false;
+	}
+
+	IEnumerator LoadScene(int level)
+    {
 		LevelController.AsyncPreLoadLevel(level);
 
-		while (LevelController.Instance.Async.progress < 0.9f)
-		{
-			yield return null;
-		}
+		yield return WhileDo(() => { return LevelController.Instance.Async.progress < 0.9f; });
 
 		LevelController.AsyncLoadLevel();
-
-		ScreenController.FadeIn(fadeIn);
 	}
+
+	IEnumerator DoFadeOut(float fadeOut)
+	{
+		ScreenController.FadeOut(fadeOut);
+
+		yield return WhileDo(() => { return ScreenController.Instance.currentFade < 1; });
+	}
+	IEnumerator DoFadeIn(float fadeIn)
+	{
+		ScreenController.FadeIn(fadeIn);
+
+		yield return WhileDo(() => { return ScreenController.Instance.currentFade >= 0; });
+	}
+
+	IEnumerator WhileDo(Func<bool> whileCondition, Action doAction = null)
+    {
+        while (whileCondition())
+        {
+			doAction?.Invoke();
+			yield return null;
+        }
+    }
 }
